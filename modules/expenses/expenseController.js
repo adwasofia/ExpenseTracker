5528d8705ec5cb98d32847e973ba9463cf88dc41
adwasofia <18220109@std.stei.itb.ac.id>
2024-07-16 14:03:41 +0700

    Add an endpoint to update an expense, including the validation and error handling

--- modules/expenses/expenseController.js
+++ modules/expenses/expenseController.js
@@ -1,5 +1,5 @@
+const { User } = require("../users/userModel");
 const Expense = require("./expenseModel");
-//const authenticateJWT = require("../../libraries/authenticator/jwtAuthenticator");
 
 const addExpense = async (req, res) => {
     const {category, amount, description, date} = req.body;
@@ -29,4 +29,39 @@ const addExpense = async (req, res) => {
 
 };
 
-module.exports = {addExpense};
+const updateExpense = async (req, res) => {
+    const user_id = req.user.id;
+    if (!user_id || !req.body.category || !req.body.amount || !req.body.description || !req.body.date) {
+        res.status(400).json({message: "User ID, Category, Amount, Description, and Date are required."});
+    }
+    let user = await User.findOne({
+        where: { id: user_id }
+    });
+    if (!user) {
+        res.status(404).json({message: "User not found."});
+    }
+    try {
+        const expense = await Expense.findOne({
+            where: {id: req.params.id, user_id: user_id}
+        });
+        if (!expense) {
+            res.status(404).json({message: "Expense not found."});
+        }
+        expense.category = req.body.category;
+        expense.amount = req.body.amount;
+        expense.description = req.body.description;
+        expense.date = req.body.date;
+        await expense.save();
+        res.status(200).json({
+            message: "An expense is successfully updated!",
+            details: expense
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: "Internal server error.",
+            details: error.message
+        });
+    }
+};
+
+module.exports = {addExpense, updateExpense};
--- modules/expenses/expenseRoutes.js
+++ modules/expenses/expenseRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const expenseRoutes = express.Router();
-const {addExpense} = require("../expenses/expenseController");
+const {addExpense, updateExpense} = require("../expenses/expenseController");
 const authenticateJWT = require("../../libraries/authenticator/jwtAuthenticator");
 
-expenseRoutes.post('/expenses', authenticateJWT, addExpense);
+expenseRoutes.post("/expenses", authenticateJWT, addExpense);
+expenseRoutes.put("/expenses/:id", authenticateJWT, updateExpense);
 
 module.exports = {expenseRoutes};
