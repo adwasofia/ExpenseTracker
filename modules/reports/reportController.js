3b66378e7374eab57f1a645bd1a24639741c2592
adwasofia <18220109@std.stei.itb.ac.id>
2024-07-17 10:11:20 +0700

    Add category report endpoint to retrieve user expenses for a specific category (user can specify the date too but its not required)

--- modules/reports/reportController.js
+++ modules/reports/reportController.js
@@ -2,7 +2,7 @@ const Expense = require("../expenses/expenseModel");
 const { Op } = require('sequelize');
 
 // Report Endpoints (Protected)
-// 1. Monthly Spending Report: GET /api/reports/monthly-spending
+// 1. Monthly Spending Report
 // Provides a report of the user's spending for a specific month.
 const monthlyReport = async (req, res) => {
     const user_id = req.user.id;
@@ -32,8 +32,36 @@ const monthlyReport = async (req, res) => {
         });
     }
 }
+
 // 2. Category-wise Spending Report: GET /api/reports/category-spending
 // Provides a report of the user's spending by category over a specified period.
+const categoryReport = async (req, res) => {
+    const user_id = req.user.id;
+    const { category, startDate, endDate  } = req.query;
+    if (!user_id || !category) {
+        return res.status(400).json({message: "User ID and Category are required."});
+    }
+    const filters = {user_id: user_id, category: category};
+    if (startDate && endDate) {
+        filters.date = {
+            [Op.between]: [new Date(startDate), new Date(endDate)]
+        }
+    }
+    try {
+        const expenses = await Expense.findAll({
+            where: filters
+        });
+        if (!expenses) {
+            return res.status(404).json({message: "Expenses not found."});
+        }
+        return res.status(200).json(expenses);
+    } catch (error) {
+        return res.status(500).json({
+            message: "Error fetching expenses.",
+            details: error.message
+        });
+    }
+}
 // 3. Yearly Spending Report: GET /api/reports/yearly-spending
 // Provides a report of the user's spending for a specific year.
 // 4. Custom Date Range Report: GET /api/reports/custom-spending
@@ -41,4 +69,4 @@ const monthlyReport = async (req, res) => {
 // 5. Top Expenses Report: GET /api/reports/top-expenses
 // Lists the top N expenses for a specified period.
 
-module.exports = {monthlyReport};
+module.exports = {monthlyReport, categoryReport};
--- modules/reports/reportRoutes.js
+++ modules/reports/reportRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const reportRoutes = express.Router();
-const {monthlyReport} = require("../reports/reportController");
+const {monthlyReport, categoryReport} = require("../reports/reportController");
 const authenticateJWT = require("../../libraries/authenticator/jwtAuthenticator");
 
 reportRoutes.get("/reports/monthly", authenticateJWT, monthlyReport);
+reportRoutes.get("/reports/category", authenticateJWT, categoryReport);
 
 module.exports = {reportRoutes};
